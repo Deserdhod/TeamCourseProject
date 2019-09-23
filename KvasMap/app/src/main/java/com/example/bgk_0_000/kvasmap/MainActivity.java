36138add29eb13ff1880f8e5ca613459203ff21b
Deserdhod <32869477+Deserdhod@users.noreply.github.com>
2019-09-23 12:48:43 +0700

    Initial commit

---
+++ KvasMap/app/src/main/java/com/example/bgk_0_000/kvasmap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bgk_0_000.kvasmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
